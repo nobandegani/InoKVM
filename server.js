83802e12b8f85d2cfe89655ea9fb320b33a16340
Inoland <contact@nobandegan.com>
2025-06-05 18:05:02 +0400

    index

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const http = require("http");
+const WebSocket = require("ws");
+const path = require("path");
+
+const app = express();
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, "public")));
+
+wss.on("connection", (ws) => {
+    console.log("Client connected");
+
+    ws.on("message", (message) => {
+        console.log(message);
+
+        // Broadcast message to all other connected clients
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === WebSocket.OPEN) {
+                client.send(message);
+            }
+        });
+    });
+
+    ws.on("close", () => {
+        console.log("Client disconnected");
+    });
+});
+
+server.listen(8080, () => {
+    console.log("Server listening at http://localhost:8080");
+});
